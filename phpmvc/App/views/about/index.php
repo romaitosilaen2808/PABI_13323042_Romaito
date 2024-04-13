dd035654510cd19bcbf02e1718ad3dff6ef61718
romaitosilaen2808 <romaitosilaen2808@gmail.com>
2024-04-13 11:34:08 +0700

    Update index.php

--- phpmvc/App/views/about/index.php
+++ phpmvc/App/views/about/index.php
@@ -1,7 +1,7 @@
 <div class="container">
     
     <h1 class="mt-4"> About me</h1>
-    <img src="<?= BASEURL; ?>/img/Gojo.jpg"  alt="Saya" width="200" class="rounded-circle shadow">
+    <img src="<?= BASEURL; ?>/img/13323042_Romaito Agustina Silaen_D3TK.JPG"  alt="Saya" width="200" class="rounded-circle shadow">
     <p> Halo nama saya <?= $data['nama']; ?> umur saya <?= $data['umur']; ?> tahun . Saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
     
 </div>
