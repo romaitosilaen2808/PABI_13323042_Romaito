38b37b64234d68af22efc5fecc15b31d8b37118d
romaitosilaen2808 <romaitosilaen2808@gmail.com>
2024-04-13 11:34:03 +0700

    Update Mahasiswa_model.php

--- phpmvc/App/models/Mahasiswa_model.php
+++ phpmvc/App/models/Mahasiswa_model.php
@@ -62,4 +62,34 @@ class Mahasiswa_model {
 
             return $this->db->rowCount();
         }
+
+        public function ubahDataMahasiswa($data){
+            $query = "UPDATE mahasiswa SET
+            Nama = :Nama,
+            Nrp = :Nrp,
+            Email = :Email,
+            Jurusan = :Jurusan,
+            WHERE Id = :Id";
+
+            $this->db->query($query);
+            $this->db->bind('Nama', $data['Nama']);
+            $this->db->bind('Nrp', $data['Nrp']);
+            $this->db->bind('Email', $data['Email']);
+            $this->db->bind('Jurusan', $data['Jurusan']);
+            $this->db->bind('Id', $data['Id']);
+
+            $this->db->execute()
+;
+
+return $this->db->rowCount();
+}
+
+public function cariDataMahasiswa()
+{
+    $keyword = $_POST['keyword'];
+    $query = "SELECT * FROM mahasiswa WHERE Nama LIKE :keyword";
+    $this->db->query($query);
+    $this->db->bind('keyword', "%$keyword%");
+    return $this->db->resultSet();
+}
 }
